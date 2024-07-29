d1880608aaaaa0414430960aa37c6dc2aac8c1fa
kayrus <kay.diam@gmail.com>
2024-07-29 10:19:10 +0200

    add missing gophercloud

--- openstack/resource_openstack_db_configuration_v1_test.go
+++ openstack/resource_openstack_db_configuration_v1_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 
+	"github.com/gophercloud/gophercloud"
 	"github.com/gophercloud/gophercloud/openstack/db/v1/configurations"
 )
 
